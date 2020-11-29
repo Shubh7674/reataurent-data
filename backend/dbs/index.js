a9bfbfe379dc443fedfd040a890d5a85ac984e68
shubham singh <shubhsingh1475@gmail.com>
2020-11-29 16:50:20 +0530

    changed user credentials

--- backend/dbs/index.js
+++ backend/dbs/index.js
@@ -2,7 +2,7 @@ const MongoClient = require('mongodb').MongoClient
 
 // Note: A production application should not expose database credentials in plain text.
 // For strategies on handling credentials, visit 12factor: https://12factor.net/config.
-const PROD_URI = "mongodb+srv://<username>:<password>@testcluster.eh5ax.mongodb.net/sample_restaurants?retryWrites=true&w=majority"
+const PROD_URI = "mongodb+srv://<usernameProd>:<password>@testcluster.eh5ax.mongodb.net/sample_restaurants?retryWrites=true&w=majority"
 // const MKTG_URI = "mongodb+srv://<username>:<password>@testcluster.eh5ax.mongodb.net/sample_restaurants?retryWrites=true&w=majority"
 
 function connect(url) {
